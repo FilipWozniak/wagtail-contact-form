e723c3e837e27bcf5beb03582996ed88329876df
FilipWozniak <filip@kachange.eu>
2024-02-09 16:27:01 +0100

    Format Tests & Change Configuration of `pre-commit`

--- contact_form/tests/unit/test_contact_page.py
+++ contact_form/tests/unit/test_contact_page.py
@@ -4,8 +4,31 @@ from wagtail.models import Site
 
 from contact_form.models import ContactPage
 from contact_form.models import FormField
-from django.core.exceptions import ValidationError
-from django.core import mail
+
+
+@pytest.fixture
+def form_submission_data():
+    return {
+        "full_name": "John Doe",
+        "email_address": "john@example.com",
+        "message": "This is a test message.",
+        "wagtailcaptcha": "03AFcWeA5-DcunrWYpbA2QAv8BVc6g9t20BOtFdu39nCUrPUL4V3iINznl1wIA0dVSN2xPIlVSHRaqElcZoc4HFEPlx_Z8QEOuBNsIxGdBjjegDI4WTIRXxyThKOUYrG1RXflAIAPL3HQPaxxDoY7ro17URTBdTtImKyyTbeiZCjfaoUpCgNtPCDIx8m8CEO9amqd_leCTsS_sE0lYihTyMVrOknRxWwFDzL0w_HHKNrR3VpehRuqiRHJ6xM1FNMP8e43gF88e-JqZIKqmRT9yn3pP59WR_ZO8QqVhNlW_N-xjfSzNFBTnHUrwv1ACzG5KrOr8Rro_audAgMyXIPqZ2A6KwYL123zNg0-QJxudvBYG4Y3fx0pE6C9x3f1z0jmfh8CgpY9Xv3mZvqDsUhWGYdHy1bQZdPT94TNA5I4bTxsI308izapbAR4QGoxrLva21DJC_-sjHgKVFtrua2996_AZ6neoN7dDC7KQmIYKM8a7BK_KGLJ-hu4U1G-v769k6rs1YQNmv8lVCp0f4Tyfs5A0tQLJwemSOimQH0fikpaY0w1uo0uyY5Bpt8irNUKlGHKfoZKuTxpHH0dDzAivsVkCP5Opha_xXGYVQ2mulW0S2TWcNouPK6E",
+    }
+
+
+# Single Line Text: "singleline"
+# Multi-Line Text: "multiline"
+# Email: "email"
+# Number: "number"
+# URL: "url"
+# Checkbox: "checkbox"
+# Checkboxes: "checkboxes"
+# Dropdown: "dropdown"
+# Multiple Select: "multiselect"
+# Radio Buttons: "radio"
+# Date: "date"
+# Date/Time: "datetime"
+# Hidden Field: "hidden"
 
 
 @pytest.mark.django_db
@@ -41,8 +64,8 @@ class TestContactPage:
         """
         assert contact_page.title == "Contact Us"
         assert (
-                contact_page.intro
-                == "We're here to help and answer any questions you might have. We look forward to hearing from you."
+            contact_page.intro
+            == "We're here to help and answer any questions you might have. We look forward to hearing from you."
         )
         assert contact_page.landing_page_template == "contact_form/contact_page_landing.html"
 
@@ -50,20 +73,6 @@ class TestContactPage:
         """
         Test the creation of form fields for the ContactPage.
         """
-        # Single Line Text: "singleline"
-        # Multi-Line Text: "multiline"
-        # Email: "email"
-        # Number: "number"
-        # URL: "url"
-        # Checkbox: "checkbox"
-        # Checkboxes: "checkboxes"
-        # Dropdown: "dropdown"
-        # Multiple Select: "multiselect"
-        # Radio Buttons: "radio"
-        # Date: "date"
-        # Date/Time: "datetime"
-        # Hidden Field: "hidden"
-
         full_name_field = FormField.objects.create(
             page=contact_page,
             sort_order=1,
@@ -97,30 +106,21 @@ class TestContactPage:
         assert message_field.field_type == "multiline"
         assert message_field.required
 
-    def test_send_form_submission_successfully(self, client, contact_page):
+        assert contact_page.form_fields.count() == 3
+
+    def test_send_form_submission_successfully(self, client, contact_page, form_submission_data):
         """
         Test the successful submission of a form on the ContactPage.
         """
-        data = {
-            "full_name": "John Doe",
-            "email_address": "john@example.com",
-            "message": "This is a test message.",
-            'wagtailcaptcha': '03AFcWeA5-DcunrWYpbA2QAv8BVc6g9t20BOtFdu39nCUrPUL4V3iINznl1wIA0dVSN2xPIlVSHRaqElcZoc4HFEPlx_Z8QEOuBNsIxGdBjjegDI4WTIRXxyThKOUYrG1RXflAIAPL3HQPaxxDoY7ro17URTBdTtImKyyTbeiZCjfaoUpCgNtPCDIx8m8CEO9amqd_leCTsS_sE0lYihTyMVrOknRxWwFDzL0w_HHKNrR3VpehRuqiRHJ6xM1FNMP8e43gF88e-JqZIKqmRT9yn3pP59WR_ZO8QqVhNlW_N-xjfSzNFBTnHUrwv1ACzG5KrOr8Rro_audAgMyXIPqZ2A6KwYL123zNg0-QJxudvBYG4Y3fx0pE6C9x3f1z0jmfh8CgpY9Xv3mZvqDsUhWGYdHy1bQZdPT94TNA5I4bTxsI308izapbAR4QGoxrLva21DJC_-sjHgKVFtrua2996_AZ6neoN7dDC7KQmIYKM8a7BK_KGLJ-hu4U1G-v769k6rs1YQNmv8lVCp0f4Tyfs5A0tQLJwemSOimQH0fikpaY0w1uo0uyY5Bpt8irNUKlGHKfoZKuTxpHH0dDzAivsVkCP5Opha_xXGYVQ2mulW0S2TWcNouPK6E'
-        }
-        response = client.post(contact_page.url, data)
+        response = client.post(contact_page.url, form_submission_data)
         assert response.status_code == 200
 
-    def test_form_submission_database_save(self, contact_page):
+    def test_form_submission_database_save(self, contact_page, form_submission_data):
         """
         Test the saving of form submission data to the database.
         """
         FormSubmission.objects.create(
-            form_data={
-                "full_name": "John Doe",
-                "email_address": "john@example.com",
-                "message": "This is a test message.",
-                'wagtailcaptcha': '03AFcWeA5-DcunrWYpbA2QAv8BVc6g9t20BOtFdu39nCUrPUL4V3iINznl1wIA0dVSN2xPIlVSHRaqElcZoc4HFEPlx_Z8QEOuBNsIxGdBjjegDI4WTIRXxyThKOUYrG1RXflAIAPL3HQPaxxDoY7ro17URTBdTtImKyyTbeiZCjfaoUpCgNtPCDIx8m8CEO9amqd_leCTsS_sE0lYihTyMVrOknRxWwFDzL0w_HHKNrR3VpehRuqiRHJ6xM1FNMP8e43gF88e-JqZIKqmRT9yn3pP59WR_ZO8QqVhNlW_N-xjfSzNFBTnHUrwv1ACzG5KrOr8Rro_audAgMyXIPqZ2A6KwYL123zNg0-QJxudvBYG4Y3fx0pE6C9x3f1z0jmfh8CgpY9Xv3mZvqDsUhWGYdHy1bQZdPT94TNA5I4bTxsI308izapbAR4QGoxrLva21DJC_-sjHgKVFtrua2996_AZ6neoN7dDC7KQmIYKM8a7BK_KGLJ-hu4U1G-v769k6rs1YQNmv8lVCp0f4Tyfs5A0tQLJwemSOimQH0fikpaY0w1uo0uyY5Bpt8irNUKlGHKfoZKuTxpHH0dDzAivsVkCP5Opha_xXGYVQ2mulW0S2TWcNouPK6E'
-            },
+            form_data=form_submission_data,
             page=contact_page,
         )
         latest_submission = FormSubmission.objects.last()
@@ -136,19 +136,11 @@ class TestContactPage:
         # settings.INSTALLED_APPS.append('cjkcms')
         response = client.get(contact_page.url)
         assert response.status_code == 200
-        assert 'base.html' in [template.name for template in response.templates]
+        assert "base.html" in [template.name for template in response.templates]
 
-    def test_form_submission_email_sent(self, client, contact_page):
+    def test_form_submission_email_sent(self, client, contact_page, form_submission_data):
         """
         Test that an email is sent when the contact form is successfully submitted.
         """
-        data = {
-            'full_name': 'John Doe',
-            'email_address': 'john@example.com',
-            'message': 'This is a test message.',
-            'wagtailcaptcha': '03AFcWeA5-DcunrWYpbA2QAv8BVc6g9t20BOtFdu39nCUrPUL4V3iINznl1wIA0dVSN2xPIlVSHRaqElcZoc4HFEPlx_Z8QEOuBNsIxGdBjjegDI4WTIRXxyThKOUYrG1RXflAIAPL3HQPaxxDoY7ro17URTBdTtImKyyTbeiZCjfaoUpCgNtPCDIx8m8CEO9amqd_leCTsS_sE0lYihTyMVrOknRxWwFDzL0w_HHKNrR3VpehRuqiRHJ6xM1FNMP8e43gF88e-JqZIKqmRT9yn3pP59WR_ZO8QqVhNlW_N-xjfSzNFBTnHUrwv1ACzG5KrOr8Rro_audAgMyXIPqZ2A6KwYL123zNg0-QJxudvBYG4Y3fx0pE6C9x3f1z0jmfh8CgpY9Xv3mZvqDsUhWGYdHy1bQZdPT94TNA5I4bTxsI308izapbAR4QGoxrLva21DJC_-sjHgKVFtrua2996_AZ6neoN7dDC7KQmIYKM8a7BK_KGLJ-hu4U1G-v769k6rs1YQNmv8lVCp0f4Tyfs5A0tQLJwemSOimQH0fikpaY0w1uo0uyY5Bpt8irNUKlGHKfoZKuTxpHH0dDzAivsVkCP5Opha_xXGYVQ2mulW0S2TWcNouPK6E'
-        }
-        response = client.post(contact_page.url, data)
-        assert response.status_code == 200
-        assert len(mail.outbox) == 1
-        assert mail.outbox[0].to == [contact_page.to_address]
+        response = client.post(contact_page.url, form_submission_data)
+        assert response.status_code
